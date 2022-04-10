d39f24ffaa046f6ec5f632a2908b86140298c7e9
Santos <wjdrms1919@gmail.com>
2022-04-11 00:10:42 +0900

    faet: franchise 1. first-section 완료

---
+++ src/features/franchise/SecondSection.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="owl-franchise-second-wrapper">
+    <div class="owl-franchise-second__title-wrapper" v-html="checkN(title)" />
+    <v-img
+      cover
+      width="100%"
+      class="owl-franchise-second__image"
+      :lazy-src="require(`@/assets/images/franchise-second1.png`)"
+      :src="require(`@/assets/images/franchise-second1.png`)"
+    />
+    <div class="owl-franchise-second__desc-wrapper">
+      <div class="owl-franchise-second__sub" v-html="checkN(subTitle)" />
+      <div class="owl-franchise-second__desc" v-html="checkN(desc)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import contents from "@/constants/franchise/second.json";
+import { lineBreak } from "@/utils/helper";
+export default {
+  computed: {
+    checkN() {
+      return (str) => lineBreak(str);
+    },
+  },
+  data() {
+    return {
+      title: contents.title,
+      subTitle: contents.subTitle,
+      desc: contents.desc,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.owl-franchise-second-wrapper {
+  $this: "owl-franchise-second";
+  width: 100%;
+  @include flex-center-column;
+  .#{$this}__title-wrapper {
+    padding-top: 80px;
+    padding-bottom: 178px;
+    @include set-text(400, 70, rgba($color: $color-title, $alpha: 1));
+    text-align: center;
+  }
+  .#{$this}__image {
+    height: 100%;
+    @include cover-background;
+  }
+
+  .#{$this}__desc-wrapper {
+    min-width: 900px;
+    padding-top: 122px;
+    .#{$this}__sub {
+      @include set-text(bold, 40, rgba($color: $color-title, $alpha: 1));
+      text-align: center;
+      display: flex;
+      flex-basis: 100%;
+      align-items: center;
+      &:after {
+        content: "";
+        flex-grow: 1;
+        height: 1px;
+        font-size: 0px;
+        line-height: 0px;
+        background-color: rgba($color: $scroll-border-color, $alpha: 1);
+        margin-left: 50px;
+      }
+      &:before {
+        content: "";
+        flex-grow: 1;
+        height: 1px;
+        font-size: 0px;
+        line-height: 0px;
+        background-color: rgba($color: $scroll-border-color, $alpha: 1);
+        margin-right: 50px;
+      }
+    }
+    .#{$this}__desc {
+      padding-top: 68px;
+      @include set-text(400, 30, rgba($color: $color-title, $alpha: 1));
+      text-align: center;
+    }
+  }
+}
+</style>
--- src/views/Franchise.vue
+++ src/views/Franchise.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="owl-franchise">
-    <div class="owl-store">
+    <div class="owl-franchise__first-section">
       <Dimmed :height="'463px'" />
       <ImageWrapper
         :imageSrc="'franchise-header-image.png'"
         title="FRANCHISE"
       />
     </div>
+    <div class="owl-franchise__second-section">
+      <SecondSection />
+    </div>
+    <div class="owl-franchise__third-section"></div>
+    <!-- <div class="owl-franchise__forth-section"></div>
+    <div class="owl-franchise__fifith-section"></div> -->
     <Banner :title="title" :desc="desc" />
   </div>
 </template>
@@ -16,12 +22,14 @@ import Dimmed from "@/components/Dimmed.vue";
 import ImageWrapper from "@/components/ImageWrappers.vue";
 import Banner from "@/components/Banner.vue";
 import contents from "@/constants/common/index.json";
+import SecondSection from "@/features/franchise/SecondSection.vue";
 
 export default {
   components: {
     Dimmed,
     ImageWrapper,
     Banner,
+    SecondSection,
   },
   data() {
     return {
@@ -32,4 +40,21 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.owl-franchise {
+  $this: "owl-franchise";
+  .#{$this}__section {
+    height: 100vh;
+  }
+  .#{$this}__first-section {
+  }
+  .#{$this}__second-section {
+    padding: 160px 273px 0 273px;
+  }
+  .#{$this}__third-section {
+    padding-top: 175px;
+  }
+  .#{$this}__forth-section {
+  }
+}
+</style>
