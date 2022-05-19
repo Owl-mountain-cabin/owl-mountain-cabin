e5e63bec8823c71dbe7e7c80a995e47216c2c173
Santos <wjdrms1919@gmail.com>
2022-05-19 23:52:16 +0900

    feat: 드로어 1. 믹스인 미디어 쿼리 생성 (모바일용) 2. 드로어 추가 3. 스타일 작업중

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -2,6 +2,39 @@
   <v-app class="owl-app-layout">
     <template v-slot:default>
       <slot name="default"></slot>
+      <v-navigation-drawer class="owl-navi-wrapper" v-model="drawer" absolute>
+        <v-list nav>
+          <v-list-item-group>
+            <v-list-item
+              active-class="owl-navi--active"
+              class="owl-navi__list-item"
+            >
+              <v-list-item-title>Foo</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item
+              active-class="owl-navi--active"
+              class="owl-navi__list-item"
+            >
+              <v-list-item-title>Bar</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item
+              active-class="owl-navi--active"
+              class="owl-navi__list-item"
+            >
+              <v-list-item-title>Fizz</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item
+              active-class="owl-navi--active"
+              class="owl-navi__list-item"
+            >
+              <v-list-item-title>Buzz</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
     </template>
   </v-app>
 </template>
@@ -9,6 +42,22 @@
 <script>
 export default {
   props: {},
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+  provide() {
+    const my = {};
+    Object.defineProperty(my, "drawer", {
+      enumerable: true,
+      get: () => this.drawer,
+      set: (value) => {
+        this.drawer = value;
+      },
+    });
+    return { my };
+  },
 };
 </script>
 <style lang="scss">
@@ -16,5 +65,17 @@ export default {
   max-width: 1920px;
   margin: 0 auto;
   background: #ffffff !important;
+  .owl-navi-wrapper {
+    z-index: 999;
+    width: 100% !important;
+    $this: "owl-navi";
+    .#{$this}--active {
+      background: transparent;
+      color: $color-accent;
+    }
+    .#{$this}__list-item {
+      text-align: center;
+    }
+  }
 }
 </style>
