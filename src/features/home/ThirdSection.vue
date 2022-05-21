3143cef90788cb0e11cfd61cf54b28831dd365d1
bnpark <bnpark@standpoint.co.kr>
2022-05-21 20:25:59 +0900

    홈/세번째섹션 모바일 반영

--- src/features/home/ThirdSection.vue
+++ src/features/home/ThirdSection.vue
@@ -57,17 +57,54 @@ export default {
   height: 100%;
   .#{$this}__contents-wrapper {
     padding: 0 140px;
+    @include mobile {
+      padding: 53px 20px 0 20px;
+      background: rgba(237, 237, 229, 0.5);
+    }
     .#{$this}__title-wrapper {
       padding: 0;
       padding-bottom: 10px;
       @include set-text(500, 60, rgba($color: $color-title, $alpha: 1));
+      @include mobile {
+        text-align: center;
+        @include set-text(600, 23, rgba($color: $color-title, $alpha: 1));
+      }
     }
     .#{$this}__desc-wrapper {
       @include set-text(400, 20, rgba($color: $color-desc, $alpha: 1));
       padding-bottom: 43px;
+      @include mobile {
+        text-align: center;
+        @include set-text(400, 16, rgba($color: $color-desc, $alpha: 1));
+      }
+      br {
+        @include mobile {
+          display: none;
+        }
+      }
     }
   }
   .#{$this}__swiper-wrapper {
+    @include mobile {
+      padding: 0 20px 53px;
+      background-color: rgba(237, 237, 229, 0.5);
+    }
+    .swiper-slide {
+      @include mobile {
+        height: 200px;
+      }
+      .v-image {
+        @include mobile {
+          height: 200px;
+        }
+        .v-image__image {
+          @include mobile {
+            height: 200px;
+            background-size: cover;
+          }
+        }
+      }
+    }
   }
 
   .#{$this}__summury-wrapper {
@@ -75,14 +112,24 @@ export default {
     align-items: center;
     justify-content: space-around;
     margin-top: 64px;
+    @include mobile {
+      margin-top: 53px;
+    }
     .#{$this}__summury-title-wrapper {
       display: flex;
       flex-direction: column;
       justify-content: center;
       text-align: center;
       @include set-text(400, 40, rgba($color: $color-title, $alpha: 1));
+      @include mobile {
+        width: 33.33%;
+        @include set-text(400, 16, rgba($color: $color-title, $alpha: 1));
+      }
       .#{$this}__summury-desc-wrapper {
         @include set-text(400, 20, rgba($color: $color-desc, $alpha: 1));
+        @include mobile {
+          @include set-text(400, 9, rgba($color: $color-desc, $alpha: 1));
+        }
       }
     }
   }
