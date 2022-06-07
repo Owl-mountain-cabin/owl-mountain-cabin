065ca71fb391c551e1a96e8fe8129d3bafbb53c5
Santos <wjdrms1919@gmail.com>
2022-06-08 00:00:47 +0900

    feat: 공통 1. 메타 태그 추가

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from "./App.vue";
 import router from "@/routes";
 import vuetify from "./plugins/vuetify";
 import "aos/dist/aos.css";
+import Meta from "vue-meta";
 
 Vue.config.productionTip = false;
-
+Vue.use(Meta, {
+  attribute: "data-vue-meta",
+});
 new Vue({
   render: (h) => h(App),
   vuetify,
